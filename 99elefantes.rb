a74d63eb8de1af432218a727f6249b0c7ee147fd
codeamac06 <codeamac06@gmail.com>
2016-09-27 19:32:49 -0500

    99elephants ruby

---
+++ 99elefantes.rb
@@ -0,0 +1,16 @@
+def elefantes
+  puts "¿En Cuantos elefantes termina la cancion?"
+  num = gets.chomp
+  
+  for i in 1..num.to_i
+    puts "#{i} elefante(s) se columpiaba(n) sobre la tela de una araña, como veía que resistía fueron a llamar a otro elefante. "
+  end
+  
+end
+
+p elefantes
+
+
+
+
+
